6c02078ed228489033132e7134b7d35ed40bcafb
Gabe Timm <gabe.m.timm@gmail.com>
2025-01-18 17:53:07 -0800

    fix: update to React 18+ rendering API

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './client/App';
 import { ThemeProvider } from './client/context/ThemeContext';
 
@@ -9,4 +9,7 @@ const AppComponent: React.FC = () => (
   </ThemeProvider>
 );
 
-ReactDOM.render(<AppComponent />, document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
+root.render(<AppComponent />);
